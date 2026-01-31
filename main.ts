2cf102ad9a66d90ced97c13f0e7fbad89c432f40
Mstry9 <163805642+mstry9@users.noreply.github.com>
2026-01-31 13:02:13 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
-import { serveFile } from "jsr:@std/http/file-server";
+import { serveDir } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+  const url = new URL(req.url);
+
+  // If the request looks like a file (has an extension), serve static file
+  if (url.pathname.includes(".")) {
+    return serveDir(req, {
+      fsRoot: ".",
+      urlRoot: "",
+    });
+  }
+
+  // Otherwise serve the main page
+  return serveDir(req, {
+    fsRoot: ".",
+    urlRoot: "",
+    index: "index.html",
+  });
 });
